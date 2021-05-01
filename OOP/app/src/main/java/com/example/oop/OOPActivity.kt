d5871b1332392d4259dff876856e820c7bb9c672
sery270 <sery270@naver.com>
2021-05-01 23:42:33 +0900

    [FEAT] ::currentMenu.isInitialized로  lateinit 버그 수정

--- OOP/app/src/main/java/com/example/oop/OOPActivity.kt
+++ OOP/app/src/main/java/com/example/oop/OOPActivity.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 
 class OOPActivity : AppCompatActivity() {
 
+    // Menu~로 정의된 클래스들은 Menu의 행동이 정의된 ScreenUI를 상속받았다.
     lateinit var currentMenu : ScreenUI
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,22 +21,38 @@ class OOPActivity : AppCompatActivity() {
         val button2 = findViewById<Button>(R.id.button2)
         val textView = findViewById<TextView>(R.id.text_view)
 
+        // 코틀린에서 함수는 값이다.
         val menuListener = View.OnClickListener{
-            when(it){
-                menu1 -> currentMenu = Menu1()
-                menu2 -> currentMenu = Menu2()
+            currentMenu = when(it){
+                menu1 -> Menu1()
+                menu2 -> Menu2()
+                else -> return@OnClickListener
             }
+
+            // ScreenUI를 통해 Menu의 행동을 정의했다.
+            // Menu인 currentMenu는 자신이 어떤 Menu인지 알필요 없이,
+            // (when의 결과에 상관 없이),
+            // Menu로써 정의된 행동을 수행하기만 하면 된다.
             currentMenu.show(textView)
         }
         val buttonListener = View.OnClickListener{
-            when(it){
-                button1 -> {
-                    currentMenu.applyButton1(textView)
+            // menuListener와는 달리 현재 클릭된 버튼이
+            // button1인지 button2인지에 따라
+            // currentMenu 에서 어떤 행동을 해야할지 달라진다.
+            // 각 Menu의 행동마다 각 button에 대한 행동이 정의되어있기 때문이다.
+            if (::currentMenu.isInitialized) {
+                when (it) {
+                    button1 -> {
+                        currentMenu.applyButton1(textView)
+                    }
+                    button2 -> {
+                        currentMenu.applyButton2(textView)
+                    }
                 }
-                button2 -> {
-                    currentMenu.applyButton2(textView)
-                }
+            } else{
+                textView.text = "메뉴를 선택해주세요."
             }
+
         }
 
         menu1.setOnClickListener(menuListener)
